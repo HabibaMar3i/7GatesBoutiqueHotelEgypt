3be22947b671128a268a657333f0c08c6a8db464
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-24 22:50:56 +0300

    fixed layout design

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -4,10 +4,12 @@ import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
     return (
-        <>
+        <div className='flex flex-col min-h-screen overflow-x-hidden'>
             <Navbar />
-            <Outlet />
+            <main className='grow'>
+                <Outlet />
+            </main>
             <Footer />
-        </>
+        </div>
     )
 }
