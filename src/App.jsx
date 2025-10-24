b925159c6ae159667aefd53638cc3c9286e93a21
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-24 16:44:33 +0300

    added routing and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import About from './pages/About/About';
+import Gallery from './pages/Gallery/Gallery';
+import Contact from './pages/Contact/Contact';
+import MainLayout from './layouts/MainLayout/MainLayout';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: '', element: <MainLayout />, children: [
+        { index: true, element: <HomePage /> },
+        { path: '/home', element: <Navigate to="/" /> },
+        { path: '/about', element: <About /> },
+        { path: '/gallery', element: <Gallery /> },
+        { path: '/contact', element: <Contact /> },
+      ]
+    },
+    { path: '*', element: <NotFound /> },
+  ])
   return (
     <>
-      <h1>Hello World</h1>
+      <RouterProvider router={routes} />
     </>
   )
 }
