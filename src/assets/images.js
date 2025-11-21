f3384fd5a015f9643e7b565162001a0792bf95c9
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-11-21 21:26:43 +0200

    removed old images and added new ones

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,36 +1,6 @@
 // Hotel Images
 import diningPyramids from './dining-pyramids.jpeg';
-import spa from './spa.jpeg';
-import room2beds from './room-2beds.jpeg';
-import bathroom from './bathroom.jpeg';
-import bathtubPyramids from './bathtub-pyramids.jpeg';
-import bathroomPyramidsView from './bathroom-pyramids-view.jpeg';
-import mosque from './mosque.jpeg';
-import pool from './pool.jpeg';
-import poolPyramids from './pool-pyramids.jpeg';
-import bedRomantic from './bed-romantic.jpeg';
-import bathtubPyramidsFlower from './bathtub-pyramids-flower.jpeg';
-import dining from './dining.jpeg';
-import shower from './showwer.jpeg';
-import pyramidsDining from './pyramids-dining.jpeg';
-import bedPyramids from './bed-pyramids.jpeg';
-import spaSauna from './spasauna.jpeg';
 
 export {
-    diningPyramids,
-    spa,
-    room2beds,
-    bathroom,
-    bathtubPyramids,
-    bathroomPyramidsView,
-    mosque,
-    pool,
-    poolPyramids,
-    bedRomantic,
-    bathtubPyramidsFlower,
-    dining,
-    shower,
-    pyramidsDining,
-    bedPyramids,
-    spaSauna
+    diningPyramids
 };
